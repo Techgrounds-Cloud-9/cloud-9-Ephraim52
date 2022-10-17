fd645f0d3d3085f548e5c5a81f0130a4f3527a78
Dominic <114390268+Ephraim52@users.noreply.github.com>
2022-10-17 15:51:52 +0200

    PRG-08
    ex1 needs image

---
+++ 03_Project/Code/Python/exercise8p1.py
@@ -0,0 +1,8 @@
+thisdict = {
+    "First name": "Casper",
+    "Last name": "Velzen",
+    "Job title": "Lead Learning Coach",
+    "Company": "Techgrounds"
+}
+for x, y in thisdict.items():
+    print(x, y)
