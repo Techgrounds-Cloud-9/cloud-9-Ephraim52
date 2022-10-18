16f66ffcd243e0d3f765e9e9aa593f8327be393d
Dominic <114390268+Ephraim52@users.noreply.github.com>
2022-10-18 14:12:08 +0200

    adjusted PRG-08
    ex8p2

--- 03_Project/Code/Python/exercise8p2.py
+++ 03_Project/Code/Python/exercise8p2.py
@@ -5,8 +5,6 @@ lastname = input("What is your last name?")
 jobfunction = input("What is your current or previous job function?")
 company = input("At which company did you work or used to work for?")
 
-header = ['first name', 'last name', 'job function', 'company']
-
 row= {
     'first name' : firstname, 
     'last name' : lastname,
@@ -14,7 +12,7 @@ row= {
     'company' : company
 }
 
-with open('python/personalinfo.csv', 'a') as csv_file:
+with open('personalinfo.csv', 'a') as csv_file:
     writer = csv.writer(csv_file)
     writer.writerow(row.keys())
     writer.writerow(row.values())
